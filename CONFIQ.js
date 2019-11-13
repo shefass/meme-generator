4dbac1e66ba7acef57c30d5a9288f2935774d0e1
Shefass <shefass@gmail.com>
2019-11-13 09:39:26 +0200

    moved addresses to CONFIQ files

---
+++ CONFIQ.js
@@ -0,0 +1,3 @@
+const WHERE_SAVE_CONVERTED_IMAGES = "./front/public/";
+
+module.exports = WHERE_SAVE_CONVERTED_IMAGES;
---
+++ front/src/CONFIQ.js
@@ -0,0 +1,5 @@
+
+//const SERVER_ADDRESS = "http://blockplay.io:4000/";
+export const FRONT_SERVER_ADDRESS = "http://localhost:3000/";
+export const SERVER_ADDRESS = "http://localhost:4000/";
+
