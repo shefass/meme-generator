203fb9b9b7e041e3d38eb9b1c53ab667e52d1d5a
shefass <shefass@gmail.com>
2019-11-25 12:46:53 +0200

    delete front, add .dockerignore

--- front/src/CONFIQ.js
+++
@@ -1,6 +0,0 @@
-
-export const SERVER_ADDRESS = "http://blockplay.io:4000/"; //there you do api calls
-export const SERVER_ADDRESS_IMAGES = "http://blockplay.io/public/"; //there you save converted images 
-export const FRONT_SERVER_ADDRESS = "http://localhost:3000/";
-export const SERVER_ADDRESS_LOCAL = "http://localhost:4000/"; //there you do api calls
-
